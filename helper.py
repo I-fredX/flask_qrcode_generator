75064a82d8f0d9a204709797a8b214d7f6d549b7
APO <igwefredrick12@gmail.com>
2024-01-13 11:19:22 +0100

    integrated helper and main

--- helper.py
+++ helper.py
@@ -1,15 +1,24 @@
 import qrcode
+"""Generates a UUID for an employee and creates a QR code encoding their name, 
+employee ID, and generated UUID.
+
+The generate_uuid function generates a random UUID for the given employee name and ID.
+
+The generate_qr_code function takes a dict with employee name, ID, and UUID, and creates a QR code 
+encoding that data. The QR code is saved to a PNG file named after the employee. The filename is returned.
+"""
 import uuid0
 
 # data = name, employee id
-def generate_id():
-    return str(uuid0.generate())
+#generate_uuid
+def generate_uuid(name:str, employee_id:str):
+    return {"name": name, "uuid": str(uuid0.generate()),"employee_id": employee_id}
 
 def generate_qr_code(data: dict) -> str:
     "This function creates and save our qrcode information"
-    unid = generate_id()
-    img = qrcode.make(str({"id": unid, "employee_id": data["employee_id"]}))
-    filename = "{}_{}.png".format(unid, data["name"]) 
+    # unid = generate_id()
+    img = qrcode.make(str({"name":data["name"],"employee_id": data["employee_id"], "unid": data["uuid"] }))
+    filename = "{}_{}.png".format( data["name"], data["uuid"]) 
     img.save(filename)
 
     # save user information to the db
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_file, jsonify
-
+import helper
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,16 +11,21 @@ def qr_code():
     try:
         json_data = request.get_json()
         if "data" in json_data:
-            
             data = json_data["data"]
-            
             name = data["name"]
             employee_id = data["employee_id"]
-            return jsonify({"name": name})
+            filename = helper.generate_qr_code(helper.generate_uuid(name, employee_id))
+            return "QR code generated successfully at {}".format(filename)
+            
         else:
             return jsonify({"message": "No data provided"})
     except Exception as e:
+        print(e)
         return jsonify({"message": "Invalid data provided"})
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
